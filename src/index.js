4a714566821bc1090f07d34733b9e0fcf027c38b
Olga Zbanok <olga@zbanok.com>
2019-10-09 01:43:41 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let one = first.split('').reverse();
+  let two = second.split('').reverse();
+
+  var res = [];
+  let temp = 0;
+  let mult = 0;
+  for (let i = 0; i < one.length; i++) {
+    for (let j = 0; j < two.length; j++) {
+      mult = (one[i] * two[j]);
+      if (res[i + j]) {
+        res[i + j] += mult;
+      } else res[i + j] = mult;
+    }
+  }
+
+  for (let i = 0; i < res.length; i++) {
+    let rest = res[i] % 10;
+    temp = Math.floor(res[i] / 10);
+    res[i] = rest;
+    if (res[i + 1]) {
+      res[i + 1] += temp;
+    } else if (temp) res[i + 1] = temp;
+  }
+  return res.reverse().join('');
 }
